4408833bb3f898d5a4753f6b9e72bce67b7cb274
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-14 03:13:13 +0530

    components added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import Moviecard from './Moviecard';
 
 function App() {
   return (
     <div className="App">
-      project setup
+    <Navbar />
+    <div className="main">
+      <div className="tabs">
+       <div className="tab">Movies</div>
+        <div className="tab" >Favorites</div> 
+        
+      </div>
+      {/* displaying data on screen */}
+      <div className="list">
+           {data.map(movie => (
+            <Moviecard movie={movie}/>
+             ) ) }
+
+        </div>
+     </div>
     </div>
+    
   );
 }
 
