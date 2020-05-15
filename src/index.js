a2e894566b9f94e0911cfe556190781997d6e876
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-16 00:16:23 +0530

    Changes made in app.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers' ;
+
+
+const store = createStore(movies);
+console.log('store' , store);
+console.log('state' , store.getState());
 
 ReactDOM.render(
   <App></App>,
